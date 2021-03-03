1d7364f355bd81605897a521beef450db552176c
mhg <mhg215@yeah.net>
2021-03-03 15:35:56 +0800

    配置README

---
+++ src/ShenNius.Share.Models/Entity/Cms/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/*************************************
+* 类 名： Article
+* 作 者： realyrare
+* 邮 箱： mhg215@yeah.net
+* 时 间： 2021/3/3 15:33:30
+* .netV： 3.1
+*┌───────────────────────────────────┐　    
+*│　   版权所有：一起牛软件　　　　	 │
+*└───────────────────────────────────┘
+**************************************/
+
+namespace ShenNius.Share.Models.Entity.Cms
+{
+    public class Article
+    {
+    }
+}
