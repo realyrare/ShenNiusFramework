3f5654bb46ee87443678b0cffdebbc27c46e60c6
mhg <mhg215@yeah.net>
2021-03-17 18:56:51 +0800

    本地图片上传

--- src/api/ShenNius.Upload.API/Controllers/UploadController.cs
+++ src/api/ShenNius.Upload.API/Controllers/UploadController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using ShenNiusSystem.Common;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ShenNius.Upload.API.Controllers
@@ -14,5 +17,31 @@ namespace ShenNius.Upload.API.Controllers
     [Authorize]
     public class UploadController : ControllerBase
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment )
+        {
+            this._hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult UploadFile()
+        {
+
+            string path = _hostingEnvironment.ContentRootPath + "\\wwwroot\\Files";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            var file = Request.Form.Files[0];
+            string fileExt = file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+            //string filename = Guid.NewGuid().ToString() + "." + fileExt;
+            //计算文件的MD5值
+            string filename2 = Md5Crypt.GetStreamMd5(file.OpenReadStream()) + "." + fileExt;
+            string fileFullName = path + "\\" + filename2;
+            using (FileStream fs = System.IO.File.Create(fileFullName))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+            //添加到数据库
+            return new JsonResult(new { IsSucceed = true, ResultMsg = "上传成功", ImgUrl = "/Files/" + filename2 });
+        }
     }
 }
