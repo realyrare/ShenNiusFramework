c0b68d5a4734029b0033432831fc0647ca357ef6
realyrare <mhg215@yeah.net>
2021-05-11 18:06:34 +0800

    新增一键匹配内链的功能

--- src/ShenNius.Share.Infrastructure/Utils/WebHelper.cs
+++ src/ShenNius.Share.Infrastructure/Utils/WebHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace ShenNius.Share.Infrastructure.Utils
+﻿using System.Text.RegularExpressions;
+
+namespace ShenNius.Share.Infrastructure.Utils
 {
     public class WebHelper
     {
@@ -18,6 +20,37 @@
                 name = nbsp + "|--" + name;
             }
             return name;
+        }
+        /// <summary>
+        /// 移除文本字符的a标签
+        /// </summary>
+        public static string ReplaceStrHref(string content)
+        {
+
+            var r = new Regex(@"<a\s+(?:(?!</a>).)*?>|</a>", RegexOptions.IgnoreCase);
+            Match m;
+            for (m = r.Match(content); m.Success; m = m.NextMatch())
+            {
+                content = content.Replace(m.Groups[0].ToString(), "");
+            }
+            return content;
+        }
+        /// <summary>
+        /// 移除字符文本Img里面Alt关键字包裹的内链
+        /// </summary>
+        public static string RemoveStrImgAlt(string content)
+        {
+            Regex rg2 = new Regex("(?<=alt=\"<a[^<]*)</a>\"");
+            if (rg2.Match(content).Success)
+            {
+                content = rg2.Replace(content, "");
+            }
+            Regex rg = new Regex("(?<=alt=\")<a href=\"[^>]*>");
+            if (rg.Match(content).Success)
+            {
+                content = rg.Replace(content, "");
+            }
+            return content;
         }
     }
 }
