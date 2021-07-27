b79e18e542408bfd623daef7987967a3d39c9055
home-mhg <mhg215@yeah.net>
2021-07-27 22:52:21 +0800

    add mvc adminclient

---
+++ src/client/ShenNius.Mvc.Admin/Controllers/Sys/RecycleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShenNius.Mvc.Admin.Controllers.Sys
+{
+    public class RecycleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/client/ShenNius.Mvc.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShenNius.Mvc.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
