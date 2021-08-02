800dea6292ebb0ee91566cda3d1a1a5ed51be2c1
realyrare <mhg215@yeah.net>
2021-08-02 17:59:27 +0800

    mvc: 角色相关界面和接口调整

--- src/client/ShenNius.Mvc.Admin/Controllers/Sys/RoleController.cs
+++ src/client/ShenNius.Mvc.Admin/Controllers/Sys/RoleController.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShenNius.Share.Domain.Services.Sys;
+using ShenNius.Share.Model.Entity.Sys;
+using System.Threading.Tasks;
+
 namespace ShenNius.Mvc.Admin.Controllers.Sys
 {
     public class RoleController : Controller
     {
+        private readonly IRoleService _roleService;
+        private readonly IR_Role_MenuService _r_Role_MenuService;
+
+        public RoleController(IRoleService roleService, IR_Role_MenuService r_Role_MenuService)
+        {
+            _roleService = roleService;
+
+            _r_Role_MenuService = r_Role_MenuService;
+        }
         [HttpGet]
         public IActionResult Index()
         {
@@ -10,9 +23,10 @@ namespace ShenNius.Mvc.Admin.Controllers.Sys
         }
         //
         [HttpGet]
-        public IActionResult Modify()
+        public async Task<IActionResult> Modify(int id=0)
         {
-            return View();
+            Role model = id==0?new Role() : await _roleService.GetModelAsync(d => d.Id == id);
+            return View(model);
         }
         [HttpGet]
         public IActionResult SetMenu()
