dcbbbb61ec26b554625efe51c9f95d4b64799924
realyrare <mhg215@yeah.net>
2021-08-02 18:10:41 +0800

    mvc:log详情调整

---
+++ src/ShenNius.Share.Infrastructure/Utils/StaticExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/*************************************
+* 类名：StaticExtension
+* 作者：realyrare
+* 邮箱：mhg215@yeah.net
+* 时间：2021/8/2 18:06:12
+*┌───────────────────────────────────┐　    
+*│　   版权所有：神牛软件　　　　	     │
+*└───────────────────────────────────┘
+**************************************/
+
+namespace ShenNius.Share.Infrastructure.Utils
+{
+    public static class StaticExtension
+    {
+        public static string ToWebString(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public static string TrimEnd(this string sourceStr, string removeStr)
+        {
+            if (string.IsNullOrEmpty(sourceStr) || !sourceStr.EndsWith(removeStr))
+            {
+                return sourceStr;
+            }
+            return sourceStr.Substring(0, sourceStr.Length - removeStr.Length);
+        }
+    }
+}
--- src/client/ShenNius.Mvc.Admin/Controllers/Sys/LogController.cs
+++ src/client/ShenNius.Mvc.Admin/Controllers/Sys/LogController.cs
@@ -1,22 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using ShenNius.Share.Domain.Services.Sys;
 using System.Threading.Tasks;
 
 namespace ShenNius.Mvc.Admin.Controllers.Sys
 {
     public class LogController : Controller
     {
+        private readonly ILogService _logService;
+        public LogController(ILogService logService)
+        {
+            _logService = logService;
+        }
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpGet]
-        public IActionResult Detail()
+        public async Task<IActionResult> Detail(int id)
         {
-            return View();
+          var model=await _logService.GetModelAsync(d => d.Id == id);
+            return View(model);
         }
         [HttpGet]
         public IActionResult Echarts()
