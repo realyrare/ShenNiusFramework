809ee07f988494ccbb1b5012bc372ffb2747f729
realyrare <mhg215@yeah.net>
2021-08-31 16:25:23 +0800

    完成小程序取消订单和确认收货

--- src/ShenNius.Share.Domain/Services/Shop/OrderGoodsService.cs
+++ src/ShenNius.Share.Domain/Services/Shop/OrderGoodsService.cs
@@ -1,5 +1,10 @@
 ﻿using ShenNius.Share.Domain.Repository;
+using ShenNius.Share.Models.Configs;
 using ShenNius.Share.Models.Entity.Shop;
+using ShenNius.Share.Models.Enums.Extension;
+using ShenNius.Share.Models.Enums.Shop;
+using System;
+using System.Threading.Tasks;
 
 /*************************************
 * 类名：OrderGoodsService
@@ -15,9 +20,37 @@ namespace ShenNius.Share.Domain.Services.Shop
 {
     public interface IOrderGoodsService : IBaseServer<OrderGoods>
     {
+        Task<ApiResult> ReceiptAsync(int orderId, int appUserId);
+        Task<ApiResult> CancelOrderAsync(int orderId, int goodsId, int appUserId);
     }
     public class OrderGoodsService : BaseServer<OrderGoods>, IOrderGoodsService
     {
+        public async Task<ApiResult> ReceiptAsync(int orderId, int appUserId)
+        {
+            await Db.Updateable<Order>().SetColumns(d => new Order() { ReceiptStatus = ReceiptStatusEnum.Received.GetValue<int>(), ReceiptTime = DateTime.Now }).Where(d => d.Id == orderId && d.AppUserId.Equals(appUserId)).ExecuteCommandAsync();
+            return new ApiResult();
+        }
+        public async Task<ApiResult> CancelOrderAsync(int orderId, int goodsId, int appUserId)
+        {
 
+            try
+            {
+                Db.Ado.BeginTran();
+
+                await Db.Deleteable<OrderGoods>().Where(d => d.OrderId == orderId && d.GoodsId == goodsId && d.AppUserId == appUserId).ExecuteCommandAsync();
+                var orderGoodsModel = await Db.Queryable<OrderGoods>().Where(d => d.OrderId == orderId && d.AppUserId == appUserId).FirstAsync();
+                if (orderGoodsModel == null)
+                {
+                    await Db.Deleteable<Order>().Where(d => d.AppUserId == appUserId && d.Id == orderId).ExecuteCommandAsync();
+                }
+                Db.Ado.CommitTran();
+                return new ApiResult();
+            }
+            catch (Exception ex)
+            {
+                Db.Ado.RollbackTran();
+                return new ApiResult(ex.Message);
+            }
+        }
     }
 }
--- src/module/ShenNius.MiniApp.API/Controllers/OrderController.cs
+++ src/module/ShenNius.MiniApp.API/Controllers/OrderController.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Mvc;
+using ShenNius.Share.Domain.Services.Shop;
+using ShenNius.Share.Models.Configs;
+using System.Threading.Tasks;
 
 namespace ShenNius.MiniApp.API.Controllers
 {
-   public class OrderController:MiniAppBaseController
+    public class OrderController:MiniAppBaseController
     {
+        private readonly IOrderService _orderService;
+        private readonly IOrderGoodsService _orderGoodsService;
+
+        public OrderController(IOrderService orderService, IOrderGoodsService orderGoodsService)
+        {
+            _orderService = orderService;
+            _orderGoodsService = orderGoodsService;
+        }
+        /// <summary>
+        /// 取消订单
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="goodsId"></param>
+        /// <returns></returns>
+        [HttpPost("cancel")]
+        public  Task<ApiResult> Cancel([FromForm] int orderId, [FromForm] int goodsId)
+        {
+            return  _orderGoodsService.CancelOrderAsync(orderId, goodsId, HttpWx.TenantId);
+        }
+        /// <summary>
+        /// 确认收货
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        [HttpPost("receipt")]
+        public  Task<ApiResult> Receipt([FromForm] int orderId)
+        {
+           return   _orderGoodsService.ReceiptAsync(orderId, HttpWx.AppUserId);
+
+        }
     }
 }
