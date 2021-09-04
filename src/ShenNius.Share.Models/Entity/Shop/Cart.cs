dc49c94dcb7b157a3249d47c9045903370fad50d
home-mhg <mhg215@yeah.net>
2021-09-04 21:29:07 +0800

    增加购物车实体

---
+++ src/ShenNius.Share.Models/Entity/Shop/Cart.cs
@@ -0,0 +1,13 @@
+﻿using ShenNius.Share.Models.Entity.Common;
+using SqlSugar;
+
+namespace ShenNius.Share.Models.Entity.Shop
+{
+    [SugarTable("shop_appuser")]
+   public class Cart : BaseTenantEntity
+    {
+        public int GoodsNum { get; set; }
+        public int AppUserId { get; set; }
+        public int GoodsId { get; set; }
+    }
+}
