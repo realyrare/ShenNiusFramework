1390c026d4cd27fcb7787da78837bec68f1dd260
realyrare <mhg215@yeah.net>
2021-12-17 16:45:11 +0800

    mvc和api program优化

--- src/ShenNius.API.Hosting/Program.cs
+++ src/ShenNius.API.Hosting/Program.cs
@@ -4,35 +4,12 @@ using Microsoft.Extensions.Hosting;
 using NLog.Web;
 using ShenNius.API.Hosting;
 
-//namespace ShenNius.API.Hosting
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            CreateHostBuilder(args).Build().Run();
-//        }
-
-//        public static IHostBuilder CreateHostBuilder(string[] args) =>
-//            Host.CreateDefaultBuilder(args)
-//                .ConfigureWebHostDefaults(webBuilder =>
-//                {
-//                    webBuilder.UseStartup<Startup>();
-//                })
-//            //1.3.0 用法
-//            //.UseServiceProviderFactory(new AspectCoreServiceProviderFactory())
-//            //用AspectCore替换默认的IOC容器
-//            .UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
-//            .UseNLog();//加入nlog日志;
-//    }
-//}
 
 
-Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-            //用AspectCore替换默认的IOC容器
-            .UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
-            .UseNLog().Build().Run();
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+{
+    webBuilder.UseStartup<Startup>();
+})
+//用AspectCore替换默认的IOC容器
+.UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
+.UseNLog().Build().Run();
--- src/ShenNius.Mvc.Admin/Program.cs
+++ src/ShenNius.Mvc.Admin/Program.cs
@@ -4,33 +4,11 @@ using Microsoft.Extensions.Hosting;
 using NLog.Web;
 using ShenNius.Mvc.Admin;
 
-//namespace ShenNius.Mvc.Admin
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            CreateHostBuilder(args).Build().Run();
-//        }
 
-//        public static IHostBuilder CreateHostBuilder(string[] args) =>
-//            Host.CreateDefaultBuilder(args)
-//                .ConfigureWebHostDefaults(webBuilder =>
-//                {
-//                    webBuilder.UseStartup<Startup>();
-//                })
-//            //用AspectCore替换默认的IOC容器
-//            .UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
-//            .UseNLog();//加入nlog日志;;
-//    }
-//}
-
-
-Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-            //用AspectCore替换默认的IOC容器
-            .UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
-            .UseNLog().Build().Run(); 
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+ {
+     webBuilder.UseStartup<Startup>();
+ })
+//用AspectCore替换默认的IOC容器
+.UseServiceProviderFactory(new DynamicProxyServiceProviderFactory())
+.UseNLog().Build().Run();
